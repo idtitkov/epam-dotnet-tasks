e6b1d0f91d30282c577da626263e35ebb4ca0ea8
Ivan Titkov <45850490+idtitkov@users.noreply.github.com>
2019-11-21 16:20:20 +0300

    Add files via upload

---
+++ NET.W.2019.Titkov.04/DoubleToBinary/DoubleToBinaryTests/BinaryConverter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoubleToBinary
+{
+    public static class BinaryConverter
+    {
+        public static string DoubleToBinary(double number)
+        {
+            long integer64bit = BitConverter.DoubleToInt64Bits(number);
+
+            return Convert.ToString(integer64bit, 2).PadLeft(64, '0');
+        }
+    }
+}
