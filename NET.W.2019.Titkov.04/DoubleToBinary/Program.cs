374def611a6e6f500f666e0ede1d77127f103492
Ivan Titkov <45850490+idtitkov@users.noreply.github.com>
2019-11-21 16:19:42 +0300

    Add files via upload

---
+++ NET.W.2019.Titkov.04/DoubleToBinary/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoubleToBinary
+{
+    /// <summary>
+    /// Main program class.
+    /// </summary>
+    public class Program
+    {
+        /// <summary>
+        /// Program entry point.
+        /// </summary>
+        public static void Main(string[] args)
+        {
+            //// Requesting number to convert.
+            double number;
+            do
+            {
+                //// Repeat until input is a number.
+                Console.Write("Enter float point number to convert: ");
+            }
+            while (!double.TryParse(Console.ReadLine(), out number));
+
+            //// Writing conversion result.
+            Console.Write("Entered number in binary: ");
+            Console.WriteLine(BinaryConverter.DoubleToBinary(number));
+
+            Console.WriteLine();
+            Console.ReadKey();
+        }
+    }
+}
