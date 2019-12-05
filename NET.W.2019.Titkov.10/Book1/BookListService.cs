2ce5c1044bb7a94a61f2683aeccdabaac05531b3
Ivan Titkov <45850490+idtitkov@users.noreply.github.com>
2019-12-05 19:15:05 +0300

    Add files via upload

---
+++ NET.W.2019.Titkov.10/Book1/BookListService.cs
@@ -0,0 +1,212 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NET.W._2019.Titkov._08
+{
+    public class BookListService
+    {
+        private const string DateFormat = "yyyy";
+        private BookListStorage bookListStorage = new BookListStorage();
+
+        /// <summary>
+        /// All possible tags to apply to book.
+        /// </summary>
+        public enum BookTags
+        {
+            Id = 1,
+            ISBN,
+            Author,
+            Title,
+            Publisher,
+            YearPublished,
+            Pages,
+            Price
+        }
+
+        /// <summary>
+        /// Adds book to storage.
+        /// </summary>
+        /// <param name="isbn">ISBN</param>
+        /// <param name="author">Author</param>
+        /// <param name="title">Title</param>
+        /// <param name="publisher">Publisher</param>
+        /// <param name="yearPublished">YearPublished</param>
+        /// <param name="pages">Pages</param>
+        /// <param name="price">Price</param>
+        public void AddBook(string isbn, string author, string title, string publisher, DateTime yearPublished, int pages, double price)
+        {
+            _ = isbn ?? throw new ArgumentException("Value should not be null", nameof(isbn));
+            _ = author ?? throw new ArgumentException("Value should not be null", nameof(author));
+            _ = title ?? throw new ArgumentException("Value should not be null", nameof(title));
+            _ = publisher ?? throw new ArgumentException("Value should not be null", nameof(publisher));
+
+            bool isBookUnique = true;
+
+            var newBook = new Book
+            {
+                Id = this.bookListStorage.Books.Count + 1,
+                ISBN = isbn,
+                Author = author,
+                Title = title,
+                Publisher = publisher,
+                YearPublished = yearPublished,
+                Pages = pages,
+                Price = price,
+            };
+
+            foreach (var existingBook in bookListStorage.Books)
+            {
+                if (newBook == existingBook)
+                {
+                    isBookUnique = false;
+                    throw new Exception("Book already exists!");
+                }
+            }
+
+            if (isBookUnique)
+            {
+                this.bookListStorage.Add(newBook);
+            }
+        }
+
+        /// <summary>
+        /// Removes book from storage by ISBN.
+        /// </summary>
+        /// <param name="isbn">ISBN</param>
+        public void RemoveBook(string isbn)
+        {
+            _ = isbn ?? throw new ArgumentException("Value should not be null", nameof(isbn));
+
+            List<Book> booksAfterRemoving = new List<Book>();
+
+            booksAfterRemoving = bookListStorage.Books.Where(x => x.ISBN != isbn).ToList();
+
+            if (booksAfterRemoving.Count == bookListStorage.Books.Count)
+            {
+                throw new Exception("Nothing to remove!");
+            }
+            else
+            {
+                bookListStorage.Books = booksAfterRemoving;
+                bookListStorage.Synchronize();
+            }
+        }
+
+        /// <summary>
+        /// Finds book by tag.
+        /// </summary>
+        /// <param name="tag">Tag</param>
+        /// <param name="value">Tag value</param>
+        /// <returns></returns>
+        public List<Book> FindBookByTag(BookTags tag, string value)
+        {
+            _ = value ?? throw new ArgumentException("Value should not be null", nameof(value));
+
+            List<Book> searchResult = new List<Book>();
+
+            switch (tag)
+            {
+                case BookTags.Id:
+                    if (int.TryParse(value, out var id))
+                    {
+                        searchResult = bookListStorage.Books.Where(b => b.Id == id).ToList();
+                    }
+
+                    break;
+                case BookTags.ISBN:
+                    searchResult = bookListStorage.Books.Where(b => b.ISBN == value).ToList();
+                    break;
+                case BookTags.Author:
+                    searchResult = bookListStorage.Books.Where(b => b.Author == value).ToList();
+                    break;
+                case BookTags.Title:
+                    searchResult = bookListStorage.Books.Where(b => b.Title == value).ToList();
+                    break;
+                case BookTags.Publisher:
+                    searchResult = bookListStorage.Books.Where(b => b.Publisher == value).ToList();
+                    break;
+                case BookTags.YearPublished:
+                    if (DateTime.TryParseExact(value, DateFormat, CultureInfo.InvariantCulture, DateTimeStyles.None, out var year))
+                    {
+                        searchResult = bookListStorage.Books.Where(b => b.YearPublished == year).ToList();
+                    }
+
+                    break;
+                case BookTags.Pages:
+                    if (int.TryParse(value, out var pages))
+                    {
+                        searchResult = bookListStorage.Books.Where(b => b.Pages == pages).ToList();
+                    }
+
+                    break;
+                case BookTags.Price:
+                    if (double.TryParse(value, out var price))
+                    {
+                        searchResult = bookListStorage.Books.Where(b => b.Price == price).ToList();
+                    }
+
+                    break;
+                default:
+                    break;
+            }
+
+            return searchResult;
+        }
+
+        /// <summary>
+        /// Sorts book by tag.
+        /// </summary>
+        /// <param name="tag">Tag</param>
+        public void SortBooksByTag(BookTags tag)
+        {
+            List<Book> sortedBooks = new List<Book>();
+
+            switch (tag)
+            {
+                case BookTags.Id:
+                    sortedBooks = bookListStorage.Books.OrderBy(b => b.Id).ToList();
+                    break;
+                case BookTags.ISBN:
+                    sortedBooks = bookListStorage.Books.OrderBy(b => b.ISBN).ToList();
+                    break;
+                case BookTags.Author:
+                    sortedBooks = bookListStorage.Books.OrderBy(b => b.Author).ToList();
+                    break;
+                case BookTags.Title:
+                    sortedBooks = bookListStorage.Books.OrderBy(b => b.Title).ToList();
+                    break;
+                case BookTags.Publisher:
+                    sortedBooks = bookListStorage.Books.OrderBy(b => b.Publisher).ToList();
+                    break;
+                case BookTags.YearPublished:
+                    sortedBooks = bookListStorage.Books.OrderBy(b => b.YearPublished).ToList();
+                    break;
+                case BookTags.Pages:
+                    sortedBooks = bookListStorage.Books.OrderBy(b => b.Pages).ToList();
+                    break;
+                case BookTags.Price:
+                    sortedBooks = bookListStorage.Books.OrderBy(b => b.Price).ToList();
+                    break;
+                default:
+                    break;
+            }
+
+            bookListStorage.Books = sortedBooks;
+
+            bookListStorage.Synchronize();
+        }
+
+        /// <summary>
+        /// Prints all storage to Console. Debugging method.
+        /// </summary>
+        public void PrintAll()
+        {
+            bookListStorage.Books.ForEach(book => Console.Write(book));
+        }
+    }
+}
---
+++ NET.W.2019.Titkov.10/Book1/BookListStorage.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NET.W._2019.Titkov._08
+{
+    public class BookListStorage
+    {
+        private readonly string filePath = @"D:\Downloads\books.dat";
+        private List<Book> books = new List<Book>();
+
+        /// <summary>
+        /// Constructor. Gain all books at startup.
+        /// </summary>
+        public BookListStorage()
+        {
+            try
+            {
+                using (BinaryReader reader = new BinaryReader(File.Open(filePath, FileMode.OpenOrCreate)))
+                {
+                    // пока не достигнут конец файла
+                    // считываем каждое значение из файла
+                    while (reader.PeekChar() > -1)
+                    {
+                        var book = new Book
+                        {
+                            Id = reader.ReadInt32(),
+                            ISBN = reader.ReadString(),
+                            Author = reader.ReadString(),
+                            Title = reader.ReadString(),
+                            Publisher = reader.ReadString(),
+                            YearPublished = DateTime.Parse(reader.ReadString()),
+                            Pages = reader.ReadInt32(),
+                            Price = reader.ReadDouble(),
+                        };
+
+                        Books.Add(book);
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+
+        /// <summary>
+        /// All books in storage.
+        /// </summary>
+        public List<Book> Books { get => books; set => books = value; }
+
+        /// <summary>
+        /// Adds book to library storage.
+        /// </summary>
+        /// <param name="book">Book to add</param>
+        public void Add(Book book)
+        {
+            _ = book ?? throw new ArgumentNullException("book should not be null", nameof(book));
+            try
+            {
+                // создаем объект BinaryWriter
+                using (BinaryWriter writer = new BinaryWriter(File.Open(filePath, FileMode.Append)))
+                {
+                    // записываем в файл значение каждого поля структуры
+                    writer.Write(book.Id);
+                    writer.Write(book.ISBN);
+                    writer.Write(book.Author);
+                    writer.Write(book.Title);
+                    writer.Write(book.Publisher);
+                    writer.Write(book.YearPublished.ToString());
+                    writer.Write(book.Pages);
+                    writer.Write(book.Price);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            Books.Add(book);
+        }
+
+        /// <summary>
+        /// Writes changes to storage.
+        /// </summary>
+        public void Synchronize()
+        {
+            try
+            {
+                // создаем объект BinaryWriter
+                using (BinaryWriter writer = new BinaryWriter(File.Open(filePath, FileMode.Create)))
+                {
+                    foreach (var book in Books)
+                    {
+                        // записываем в файл значение каждого поля структуры
+                        writer.Write(book.Id);
+                        writer.Write(book.ISBN);
+                        writer.Write(book.Author);
+                        writer.Write(book.Title);
+                        writer.Write(book.Publisher);
+                        writer.Write(book.YearPublished.ToString());
+                        writer.Write(book.Pages);
+                        writer.Write(book.Price);
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
