d221ed235ae41c506d294d4d557b1f692cbdecbb
Ivan Titkov <45850490+idtitkov@users.noreply.github.com>
2020-01-17 03:55:59 +0300

    Add files via upload

---
+++ NET.W.2019.Titkov.20/DAL.EF/BankAccountContext.cs
@@ -0,0 +1,17 @@
+﻿namespace DAL.EF
+{
+    using System.Data.Entity;
+    using DAL.EF.Entities;
+
+    public class BankAccountContext : DbContext
+    {
+        public BankAccountContext() : base("Accounts")
+        {
+            Database.SetInitializer<BankAccountContext>(new DropCreateDatabaseAlways<BankAccountContext>());
+        }
+
+        public DbSet<AccountEF> Accounts { get; set; }
+
+        public DbSet<AccountTypeEF> AccountTypes { get; set; }
+    }
+}
