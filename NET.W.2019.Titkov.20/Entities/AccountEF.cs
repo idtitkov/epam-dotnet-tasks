c011c3b0819b4754f12c66b30d0818b2d926d099
Ivan Titkov <45850490+idtitkov@users.noreply.github.com>
2020-01-17 03:52:36 +0300

    Add files via upload

---
+++ NET.W.2019.Titkov.20/Entities/AccountEF.cs
@@ -0,0 +1,24 @@
+﻿namespace DAL.EF.Entities
+{
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+
+    public class AccountEF
+    {
+        [Key]
+        public string AccountId { get; set; }
+
+        public string LastName { get; set; }
+
+        public string FirstName { get; set; }
+
+        public decimal Balance { get; set; }
+
+        public int Bonus { get; set; }
+        
+        public int AccountTypeId { get; set; }
+
+        [ForeignKey("AccountTypeId")]
+        public virtual AccountTypeEF AccountType { get; set; }
+    }
+}
---
+++ NET.W.2019.Titkov.20/Entities/AccountTypeEF.cs
@@ -0,0 +1,12 @@
+﻿namespace DAL.EF.Entities
+{
+    using System.ComponentModel.DataAnnotations;
+
+    public class AccountTypeEF
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public int Type { get; set; }
+    }
+}
